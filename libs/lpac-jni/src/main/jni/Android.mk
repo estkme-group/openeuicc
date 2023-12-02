c7b393c5fbdb5ec5f03de114104666c9b17bada2
Peter Cai <peter@typeblog.net>
2023-12-02 14:13:02 -0500

    Revert "fix: jni build on windows"
    This reverts commit 195861f24bfa64d86799b717b0e284ff6611c88d.

--- libs/lpac-jni/src/main/jni/Android.mk
+++ libs/lpac-jni/src/main/jni/Android.mk
@@ -1,29 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 # function to find all *.c files under a directory
-# Detecting the operating system
-ifeq ($(OS),Windows_NT)
-# Windows-specific commands
 define all-c-files-under
-  $(patsubst .\%,%, \
-    $(shell cd $(LOCAL_PATH) & \
-            dir /b/s $(subst /,\,$(1))\*.c) \
-   )
+$(patsubst ./%,%, \
+  $(shell cd $(LOCAL_PATH) ; \
+          find $(1) -name "*.c" -and -not -name ".*" -maxdepth 1) \
+ )
 endef
-else
-# UNIX commands
-define all-c-files-under
-  $(patsubst ./%,%, \
-    $(shell cd $(LOCAL_PATH) ; \
-            find $(1) -name "*.c" -and -not -name ".*" -maxdepth 1) \
-   )
-endef
-endif
 
 include $(CLEAR_VARS)
-LOCAL_SHORT_COMMANDS := true
-APP_SHORT_COMMANDS := true
-
 # libcjson
 LOCAL_MODULE := lpac-cjson
 LOCAL_SRC_FILES := \
@@ -31,9 +16,6 @@ LOCAL_SRC_FILES := \
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SHORT_COMMANDS := true
-APP_SHORT_COMMANDS := true
-
 # libasn1c, the ASN parser component from lpac
 LOCAL_MODULE := lpac-asn1c
 LOCAL_C_INCLUDES := \
@@ -44,9 +26,6 @@ LOCAL_CFLAGS := -DHAVE_CONFIG_H
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SHORT_COMMANDS := true
-APP_SHORT_COMMANDS := true
-
 # libeuicc component from lpac, which contains the actual implementation
 LOCAL_MODULE := lpac-euicc
 LOCAL_STATIC_LIBRARIES := lpac-asn1c lpac-cjson
@@ -57,9 +36,6 @@ LOCAL_SRC_FILES := \
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SHORT_COMMANDS := true
-APP_SHORT_COMMANDS := true
-
 LOCAL_MODULE := lpac-jni
 LOCAL_STATIC_LIBRARIES := lpac-euicc
 LOCAL_C_INCLUDES := \
