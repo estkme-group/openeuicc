163ee279185bc113e2362f2a3a4df6b1bb913c9c
Peter Cai <peter@typeblog.net>
2024-01-31 21:07:37 -0500

    LuiActivity: implement a simple UI
    This is needed to unblock SetupWizard.

--- app/src/main/java/im/angry/openeuicc/ui/LuiActivity.kt
+++ app/src/main/java/im/angry/openeuicc/ui/LuiActivity.kt
@@ -1,12 +1,21 @@
 package im.angry.openeuicc.ui
 
 import android.content.Intent
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import im.angry.openeuicc.R
 
 class LuiActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
-        startActivity(Intent(this, PrivilegedMainActivity::class.java))
-        finish()
+        setContentView(R.layout.activity_lui)
+
+        findViewById<View>(R.id.lui_skip).setOnClickListener { finish() }
+        // TODO: Allow users to select slots, and then hand over directly to ProfileDownloadFragment
+        // TODO: Deactivate LuiActivity if there is no eSIM found.
+        // TODO: Support pre-filled download info (from carrier apps); UX
+        findViewById<View>(R.id.lui_download).setOnClickListener {
+            startActivity(Intent(this, PrivilegedMainActivity::class.java))
+        }
     }
 }
