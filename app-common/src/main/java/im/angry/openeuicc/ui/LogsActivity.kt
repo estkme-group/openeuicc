e48a919335583f1f86d6955a1fc41ce42bb219ee
Peter Cai <peter@typeblog.net>
2024-03-03 13:22:46 -0500

    feat: Allow exporting logs as txt

--- app-common/src/main/java/im/angry/openeuicc/ui/LogsActivity.kt
+++ app-common/src/main/java/im/angry/openeuicc/ui/LogsActivity.kt
@@ -1,9 +1,13 @@
 package im.angry.openeuicc.ui
 
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.ScrollView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
@@ -12,11 +16,23 @@ import im.angry.openeuicc.util.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.FileOutputStream
+import java.util.Date
 
 class LogsActivity : AppCompatActivity() {
     private lateinit var swipeRefresh: SwipeRefreshLayout
     private lateinit var scrollView: ScrollView
     private lateinit var logText: TextView
+
+    private val saveLogs =
+        registerForActivityResult(ActivityResultContracts.CreateDocument("text/plain")) { uri ->
+            if (uri == null) return@registerForActivityResult
+            contentResolver.openFileDescriptor(uri, "w")?.use {
+                FileOutputStream(it.fileDescriptor).use { os ->
+                    os.write(logText.text.toString().encodeToByteArray())
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +56,21 @@ class LogsActivity : AppCompatActivity() {
         lifecycleScope.launch {
             reload()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_logs, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.save -> {
+            saveLogs.launch(getString(R.string.logs_filename_template,
+                SimpleDateFormat.getDateTimeInstance().format(Date())
+            ))
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 
     private suspend fun reload() = withContext(Dispatchers.Main) {
