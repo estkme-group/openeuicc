f99bed4327383cca78c1391f83004a38687350c6
Peter Cai <peter@typeblog.net>
2026-07-04 16:58:35 -0400

    feat: priv official website preference

--- app/src/main/java/im/angry/openeuicc/di/PrivilegedCustomizableTextProvider.kt
+++ app/src/main/java/im/angry/openeuicc/di/PrivilegedCustomizableTextProvider.kt
@@ -1,10 +1,21 @@
 package im.angry.openeuicc.di
 
 import android.content.Context
+import android.net.Uri
+import androidx.core.net.toUri
+import androidx.core.os.ConfigurationCompat
 import im.angry.openeuicc.R
 
 class PrivilegedCustomizableTextProvider(private val context: Context) :
     DefaultCustomizableTextProvider(context) {
+    override val websiteUri: Uri
+        get() {
+            val language = ConfigurationCompat.getLocales(context.resources.configuration).get(0)?.toLanguageTag() ?: ""
+            return context.getString(R.string.pref_info_website_url).toUri().buildUpon()
+                .appendQueryParameter("hl", language) // host language for localized website
+                .build()
+        }
+
     override val noEuiccExplanation: String
         get() = context.getString(R.string.no_euicc_priv)
 }
