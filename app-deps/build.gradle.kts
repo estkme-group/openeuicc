d452fc03bb84afd75c25bb9b40783d47dfd2684b
Peter Cai <peter@typeblog.net>
2026-08-03 18:04:07 -0400

    chore: Bump dependency versions
    Either matching AOSP 17 or slightly lower due to AOSP 17 including alpha
    releases.

--- app-deps/build.gradle.kts
+++ app-deps/build.gradle.kts
@@ -13,7 +13,7 @@ apply {
 
 android {
     namespace = "im.angry.openeuicc_deps"
-    compileSdk = 34
+    compileSdk = 37
 
     defaultConfig {
         minSdk = 28
@@ -41,22 +41,22 @@ android {
 }
 
 dependencies {
-    api("androidx.core:core-ktx:1.12.0")
-    api("androidx.appcompat:appcompat:1.6.1")
-    api("com.google.android.material:material:1.10.0")
-    api("androidx.constraintlayout:constraintlayout:2.1.4")
+    api("androidx.core:core-ktx:1.19.0")
+    api("androidx.appcompat:appcompat:1.7.1")
+    api("com.google.android.material:material:1.14.0")
+    api("androidx.constraintlayout:constraintlayout:2.2.2")
     //noinspection KtxExtensionAvailable
     api("androidx.preference:preference:1.2.1")
-    api("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    api("androidx.lifecycle:lifecycle-service:2.6.2")
-    api("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    api("androidx.lifecycle:lifecycle-runtime-ktx:2.11.0")
+    api("androidx.lifecycle:lifecycle-service:2.11.0")
+    api("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0")
     api("androidx.cardview:cardview:1.0.0")
     api("androidx.viewpager2:viewpager2:1.1.0")
-    api("androidx.datastore:datastore-preferences:1.0.0")
+    api("androidx.datastore:datastore-preferences:1.2.1")
     api("com.journeyapps:zxing-android-embedded:4.3.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
 
 configure<GenerateBpPluginExtension> {
